ec5624d955927b2b4bcda02ecbf6726e1e7cde4b
Scofield62 <scofield62@gmail.com>
2019-04-22 18:14:51 +0200

    Creation of NGINXPHP

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Usage of nginxphp
+
+* cd docker
+* docker-compose up -d
+
+> Put the sourcecode into the source directory, even if the container has been started ;)
---
+++ source/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Welcome to NGINXPHP</title>
+</head>
+<body>
+<?php
+
+print "<h1 style='text-align: center;'>Welcome to NGINXPHP in Containerized world</h1>";
+phpinfo();
+
+?>
+</body>
+</html>
